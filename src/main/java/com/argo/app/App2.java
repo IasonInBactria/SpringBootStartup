1fb4ae8a09b1c69b4d14d1bc3982eba6c7f5a96c
Yong Yang <yangy153@chinatelecom.cn>
2023-11-08 17:35:11 +0800

    该配置

--- src/main/java/com/argo/app/App2.java
+++ src/main/java/com/argo/app/App2.java
@@ -3,11 +3,9 @@ package com.argo.app;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App1 {
+public class App2 {
     public static void main(String[] args){
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext2.xml");
-        Object cat = ctx.getBean("cat");
-        System.out.println("cat");
         String[] names = ctx.getBeanDefinitionNames();
         for (String name: names){
             System.out.println(name);
